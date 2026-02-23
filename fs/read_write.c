1d2b1e3dc16cc671173cc6bfc0bc2be20c463cab
Dan <daniel.percifield4561@gmail.com>
2026-02-23 13:15:54 -0800

    Update read_write.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -453,6 +453,9 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
 
+	/* --- KernelSU-Next-Legacy Hook --- */
+	ksu_handle_vfs_read(&file, &buf, &count, pos);
+
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
 	if (!(file->f_mode & FMODE_CAN_READ))
@@ -460,8 +463,7 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 	if (unlikely(!access_ok(buf, count)))
 		return -EFAULT;
 
-	/* --- KernelSU-Next-Legacy Hook --- */
-	ksu_handle_vfs_read(file, buf, count, pos);
+	
 	
 	ret = rw_verify_area(READ, file, pos, count);
 	if (!ret) {
